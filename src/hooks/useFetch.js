a743d44009a05bdd1989c03040ee49b8578c4660
zae21 <prayoga96@gmail.com>
2022-12-09 08:39:34 +0700

    add lesson 24

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,29 +1,39 @@
 import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-    
+
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(null);
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error("Filed to fatch data from server");
-                }
-                return res.json();
-            })
-            .then(data => {
-                setIsError(null);
-                setData(data);
-                setIsLoading(false);
-            })
-            .catch(err => {
-                setIsError(err.message);
-                setIsLoading(false);
-            });
-    }, []);
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("Filed to fatch data from server");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setIsError(null);
+                    setData(data);
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    if (err.name === "AbortError") {
+                        console.log(err.message);
+                    } else {
+                        setIsError(err.message);
+                        setIsLoading(false);
+                    }
+                });
+        }, 1000);
+
+        return () => abortCont.abort();
+    }, [url]);
 
     return { isLoading, data, isError }
 }
